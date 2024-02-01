6223421a002bd44a7e0314c806ff23e253350df7
DmitryPavlikoskiy <getuch375@gmail.com>
2024-02-01 15:50:26 +0200

    uncommitted function BasketDelete()

--- HimiaSite/static/js/redirect_home_page.js
+++ HimiaSite/static/js/redirect_home_page.js
@@ -12,29 +12,29 @@ function RedirectFromLiqPayToHomePage(){
 
 RedirectFromLiqPayToHomePage()
 
-// function BasketDelete() {
-//     let user_or_anonymousUser = document.getElementById("user_or_anonymousUser").value;  // отримуємо значення, не сам елемент
-//     let URL = '/basket/delete_basket/';
-//     const csrftoken = getCsrfToken();
-//
-//     let data = {
-//         session_or_user_id: user_or_anonymousUser,
-//         order_id: order,
-//     };
-//
-//     $.ajax({
-//         type: 'POST',
-//         url: URL,
-//         headers: {
-//             'X-CSRFToken': csrftoken,
-//             'Content-Type': 'application/json',
-//         },
-//         data: JSON.stringify(data),
-//         success: function (response) {
-//             console.log('Basket deleted successfully:', response);
-//         },
-//         error: function (error) {
-//             console.error('Error deleting basket:', error);
-//         },
-//     });
-// }
+function BasketDelete() {
+    let user_or_anonymousUser = document.getElementById("user_or_anonymousUser").value;  // отримуємо значення, не сам елемент
+    let URL = '/basket/delete_basket/';
+    const csrftoken = getCsrfToken();
+
+    let data = {
+        session_or_user_id: user_or_anonymousUser,
+        order_id: order,
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: URL,
+        headers: {
+            'X-CSRFToken': csrftoken,
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(data),
+        success: function (response) {
+            console.log('Basket deleted successfully:', response);
+        },
+        error: function (error) {
+            console.error('Error deleting basket:', error);
+        },
+    });
+}
